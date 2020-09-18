69f9dea8e97224a9ce85330dcd710689061e1295
k4zam1 <xlinux816@gmail.com>
2020-09-19 00:53:37 +0900

    :bread: add breadcrumb list

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -14,6 +14,7 @@
                     <li class="nav-item"><a class="nav-link" href="#">Top</a></li>
                     <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
                     <li class="nav-item"><a class="nav-link" href="#work">Work</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
                 </ul>
             </div>
         </div>
